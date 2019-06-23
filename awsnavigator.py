e85eb9645d2040718c64dbd299e3d483877f2c76
rajesh <gudur@pega.com>
2019-06-24 00:56:46 +0530

    s3 topup

--- awsnavigator.py
+++ awsnavigator.py
@@ -3,63 +3,53 @@ import boto3
 import sys
 import re
 import argparse
-import importlib
 from importlib.machinery import SourceFileLoader
 import traceback
-def get_related_object (SelectedService):
-	module = SourceFileLoader(SelectedService,"classes/%s.py" % (SelectedService)).load_module()
-	class_ = getattr(module, SelectedService)
-	instance = class_()
-	return instance
 
-SupportedServices = ['s3']
-SupportedCommands = ['list','open', 'cd', 'ls']
-SupportedItems = {
-	'iam': ['users'],
-	's3': ['buckets']
-}
-
-SelectedService = ''
-PresentPath = ''
+supportedServices = ['s3']
+selectedService = ''
+presentPath = []
 session = None
 region = None
+module = None
 
 def readInput(*args):
 	try:
 		raw_input = ""
-
 		prompt = args[0]
 		commands = args[1]
+		commands.append('exit')
 		regExp = "^(" + "|".join(commands) + ")$"
-
 		exp = re.compile(regExp)
 
 		while True:
 			raw_input = input(prompt)
 			raw_input = raw_input.strip()
-
 			if raw_input == "":
-				print("Please enter a valid value from the list ("+", ".join(commands)+") :")
+				print("Please enter a valid command from the list ("+", ".join(commands)+")")
 				continue
 
 			words = re.split('\s+', raw_input)
-
 			m = exp.search(words[0])
+			if words[0] == 'exit':
+				print('Exiting the Navigator')
+				sys.exit()
 
 			if m:
 				return words
 
 			if raw_input != "":
-				print("Invalid input format: " + raw_input)
+				print("Invalid input: " + raw_input)
+				print("Please enter a valid command from the list ("+", ".join(commands)+")")
 
 	except Exception as e:
 		raise e
 
-def listBuckets(session):
-	client = session.client('s3')
-	buckets = client.list_buckets()
-	for bucket in buckets['Buckets']:
-		print(bucket['Name'])
+def getRelatedObject (selectedService):
+	module = SourceFileLoader(selectedService,"classes/%s.py" % (selectedService)).load_module()
+	class_ = getattr(module, selectedService)
+	instance = class_()
+	return instance
 
 def isValidRegion(region, service):
 	if region in session.get_available_regions(service):
@@ -70,12 +60,15 @@ def isValidRegion(region, service):
 def main(args):
 	global session
 	global region
-	global SelectedService
-	global PresentPath
+	global selectedService
+	global presentPath
+	global module
 
 	try:
+
 		session = boto3.Session(profile_name=args.profile)
-		if session.region_name or args.region !='None':
+
+		if session.region_name or args.region !='':
 			pass
 		else:
 			print('Please Update region in AWS Config or pass it as a Parameter to AWSNavigator')
@@ -84,22 +77,30 @@ def main(args):
 		region = session.region_name if args.region == 'None' else args.region
 
 		while True:
-			if SelectedService == '':
-				Service = readInput('Select Service :', SupportedServices)
-				SelectedService = Service[0]
-				if isValidRegion(region,SelectedService):
+			if selectedService == '':
+				Service = readInput('Select Service :', supportedServices)
+				selectedService = Service[0]
+				if isValidRegion(region,selectedService):
 					pass
 				else:
 					print("%s is not present in %s, defaulting to %s" % (Service[0],args.region,session.region_name))
+			try:
+				if module.getServiceName() != selectedService:
+					module = getRelatedObject(selectedService)
+					module.setup(session)
+			except Exception as e:
+				if format(e) == "'NoneType' object has no attribute 'getServiceName'":
+					module = getRelatedObject(selectedService)
+					module.setup(session)
+				else:
+					print(e)
+			params = readInput("%s:/%s :" % (selectedService, "/".join(presentPath)), module.supportedCommands())
+			module.call(presentPath,params)
+			presentPath = module.returnParams('presentPath')
+			selectedService = module.returnParams('selectedService')
 
-			module = get_related_object(SelectedService)
-			print(module.call())
-			readInput("/%s/%s :" % (SelectedService, PresentPath), SupportedCommands)
-			print("hello")
-			sys.exit()
 	except Exception as e:
 		raise e
-
 
 try:
 	if __name__ == "__main__":
@@ -107,9 +108,9 @@ try:
 			raise Exception("OS not Supported")
 		parser = argparse.ArgumentParser(description='AWS Navigator')
 		parser.add_argument('--profile', dest='profile',type=str,default='default',help='AWS Profile')
-		parser.add_argument('--region', dest='region',type=str,help='AWS Region Name')
+		parser.add_argument('--region', dest='region',type=str,default='None',help='AWS Region Name')
 		args = parser.parse_args()
 		main(args)
 except Exception as e:
 	print(e)
-	traceback.print_tb(e.original.__traceback__)
+	traceback.print_exc()
--- classes/s3.py
+++ classes/s3.py
@@ -1,3 +1,140 @@
-class s3:
-	def call(self):
-		return "hello s3"
+from importlib.machinery import SourceFileLoader
+import re
+import os
+
+module = SourceFileLoader("","classes/base.py").load_module()
+class_ = getattr(module, 'baseModule')
+
+class s3 (class_):
+	moduleName = 's3'
+	selectedService = 's3'
+	presentPath = []
+	buckets = []
+	bucketsFetched = False
+	bucket = ''
+	items = []
+
+	emptyPathFunctions = {
+		'list': 'listBuckets',
+		'open': 'openBucket', 
+		'cd': 'changePath',
+		'ls': 'listBuckets'
+		}
+
+	PathFunctions = {
+		'list': 'listObjects',
+		'cd': 'changePath',
+		'ls': 'listObjects',
+		'presign': 'preSign'
+		}
+
+	def openBucket(self):
+		try:
+			if self.params[1].count('/') == 0:
+				bucket = self.params[1]
+			else:
+				firstItem = self.params[1].find('/')
+				bucket = self.params[1][0:firstItem]
+				key = self.params[1][firstItem+1:]
+				print("%s,%s" % (bucket,key))
+				print(len(key))
+		except IndexError as error:
+			print('path not found')
+		except Exception as e:
+			raise e
+
+	def starterFunctions(self):
+		return True
+
+	def fetchBuckets(self):
+		buckets = self.client.list_buckets()
+		for bucket in buckets['Buckets']:
+			self.buckets.append(bucket['Name'])
+		self.bucketsFetched = True
+
+	def getFunction(self, command):
+		if len(self.presentPath) == 0:
+			function = self.emptyPathFunctions.get(command,'none')
+			return getattr(self, function)
+		else:
+			function = self.PathFunctions.get(command,'none')
+			return getattr(self, function)
+
+
+	def call(self,presentPath,params):
+		self.presentPath = presentPath
+		self.params = params
+		print(params)
+		function_ = self.getFunction(params[0])
+		function_()
+
+	def returnParams(self, param):
+		return getattr(self, param)
+
+	def supportedCommands(self):
+		if len(self.presentPath) == 0:
+			return list(self.emptyPathFunctions.keys())
+		else:
+			return list(self.PathFunctions.keys())
+
+	def changePath (self):
+		if len(self.params) >= 2:
+			if self.params[1] == '..':
+				try:
+					self.presentPath.pop()
+				except IndexError as error:
+					print('Already present at root, cant go back further')
+				except Exception as e:
+					print(e)
+			elif self.params[1] == '/':
+				self.presentPath = []
+			elif self.params[1].count('/') > 0:
+				items = re.split('/',self.params[1])
+				for item in items:
+					if self.verifyPath(item):
+						self.presentPath.append(item)
+					else:
+						print("Path not found after s3://"+"/".join(self.presentPath))
+			else:
+				if self.verifyPath(self.params[1]):
+					self.presentPath.append(self.params[1])
+				else:
+					print("Path not found after s3://"+"/".join(self.presentPath))
+		
+		if self.bucket == "" and len(self.presentPath) > 0:
+			self.bucket = self.presentPath[0]
+		if len(self.presentPath) == 0:
+			self.bucket = ""
+
+
+	def listObjects(self):
+		command = "aws s3 ls s3://"+"/".join(self.presentPath)+"/"
+		output = os.popen(command).read().split("\n")
+		for item in output:
+			print(item.strip())
+		# output = [item.strip() for item in output]
+
+	def verifyPath(self, item):
+		command = "aws s3 ls s3://"+"/".join(self.presentPath+[item])
+		output = os.popen(command).read()
+		print(output)
+		if output != "":
+			return True
+		else:
+			return False
+	def listBuckets(self):
+		self.fetchBuckets()
+		for bucket in self.buckets:
+			print(bucket)
+
+	def getBucketRegion(self):
+		self.resource.meta.client.get_bucket_location(Bucket=self.bucket)["LocationConstraint"]
+
+	def preSign(self):
+		if len(self.params) > 1:
+			command = "aws s3 presign "+"/".join(self.presentPath)+"/"+self.params[1] + " --region " + self.resource.meta.client.get_bucket_location(Bucket=self.bucket)["LocationConstraint"]
+			output = os.popen(command).read()
+			print(output)
+		else:
+			print("insufficient arguments, format: presign <itemname>, note: item should be present in current folder")
+
